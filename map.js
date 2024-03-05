81eb3473fca1693627c26a9bb0c824adee035d41
leozamboni <leonardonunes169@gmail.com>
2024-03-05 14:25:01 -0300

    new ui

--- map.js
+++ map.js
@@ -2,13 +2,13 @@ export const places = [
   {
     id: "church_of_st_peter_stourton",
     span: "St Peter's Church, Stourton.",
-    img: "./images/places/st-peters-church-stourton.jpg",
-    model: "models/church_of_st_peter_stourton",
+    img: "img/places/st-peters-church-stourton.jpg",
+    model: "3d/church_of_st_peter_stourton",
   },
 ];
 export const rosaries = [
   {
     id: "wood_default",
-    model: "models/rosary",
+    model: "3d/rosary",
   },
 ];
